6a233683bfcc0372c36b24cd7d9cfe07071b76f2
Kim Morrison <kim@tqft.net>
2025-07-31 05:13:54 +0200

    .

--- Mathlib/Data/Rat/Intervals.lean
+++ Mathlib/Data/Rat/Intervals.lean
@@ -1,3 +1,5 @@
+import Mathlib.Algebra.Order.Ring.Unbundled.Rat
+import Mathlib.Data.Nat.Cast.Order.Basic
 import Mathlib
 
 class LERat (α : Type) where
@@ -19,10 +21,16 @@ instance : LERat Nat where
   leRat a b := a ≤ b
   ratLE a b := a ≤ b
 
+theorem _root_.Rat.natCast_le_natCast {a b : Nat} : (a : Rat) ≤ (b : Rat) ↔ a ≤ b := Nat.cast_le
+
 instance : RatCompare Nat where
-  leRat_le {a b c} hab hbc := @le_trans _ _ (a : Rat) b c hab hbc
-  le_leRat := sorry
-  le_ratLE := sorry
-  ratLE_le := sorry
+  leRat_le {a b c} hab hbc :=
+    decide_eq_true (Rat.le_trans (of_decide_eq_true hab) hbc)
+  le_leRat {a b c} hab hbc :=
+    decide_eq_true (Rat.le_trans (Rat.natCast_le_natCast.mpr hab) (of_decide_eq_true hbc))
+  le_ratLE {a b c} hab hbc :=
+    decide_eq_true (Rat.le_trans hab (of_decide_eq_true hbc))
+  ratLE_le {a b c} hab hbc :=
+    decide_eq_true (Rat.le_trans (of_decide_eq_true hab) (Rat.natCast_le_natCast.mpr hbc))
 
 end LERat
