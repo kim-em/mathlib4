5e10ea1ac78a039e902361b43a02f5ca9fcaded3
Kim Morrison <kim@tqft.net>
2025-08-28 11:27:02 +1000

    comments

--- Mathlib/Analysis/SpecialFunctions/Trigonometric/Intervals.lean
+++ Mathlib/Analysis/SpecialFunctions/Trigonometric/Intervals.lean
@@ -1,108 +1,55 @@
 import Mathlib
 
--- https://github.com/leanprover-community/mathlib4/pull/29022
-attribute [fun_prop] Real.contDiff_sin Real.contDiff_cos
+/-!
+# Examples of building interval propagators.
+-/
 
-section -- https://github.com/leanprover-community/mathlib4/pull/29023
+/--
+A machine for propagating intervals through a function.
+(The interface here will certainly change.)
 
-@[simp]
-theorem iteratedDeriv_add_one_sin (n : ℕ) :
-    iteratedDeriv (n + 1) Real.sin = iteratedDeriv n Real.cos := by
-  induction n with
-  | zero => simp only [zero_add, iteratedDeriv_one, Real.deriv_sin, iteratedDeriv_zero]
-  | succ n ih =>
-    rw [iteratedDeriv_succ, ih, iteratedDeriv_succ]
+The propagator is only required to propagate subintervals of the declared `Set.Icc a b`.
 
-@[simp]
-theorem iteratedDeriv_add_one_cos (n : ℕ) :
-    iteratedDeriv (n + 1) Real.cos = - iteratedDeriv n Real.sin := by
-  induction n with
-  | zero => ext; simp
-  | succ n ih =>
-    rw [iteratedDeriv_succ, ih, iteratedDeriv_succ, deriv.neg']
-    ext x
-    simp
+* `forward q x y h` must produce a pair of rationals `(r, s)` such that for `z : ℝ`
+  if `z ∈ Set.Icc x y` then `f x ∈ Set.Icc r s`
 
-@[simp]
-theorem iteratedDeriv_even_sin (n : ℕ) :
-    iteratedDeriv (2 * n) Real.sin = (-1) ^ n * Real.sin := by
-  induction n with
-  | zero => simp
-  | succ n ih => simp_all [mul_add, pow_succ]
+The parameter `q` is a "quality" parameter (smaller is better),
+which is used to control the effort expended in producing a tight interval.
+No guarantees are required regarding the dependence on `q`,
+but we suggest that where possible the additional error introduced should be bounded by `q`.
+-/
+structure IntervalPropagator (f : ℝ → ℝ) (a b : ℚ) where
+  forward (q : ℚ) (x : ℚ) (y : ℚ) (h : Set.Icc x y ⊆ Set.Icc a b) : ℚ × ℚ
+  mem (q : ℚ) (x : ℚ) (y : ℚ) (h : Set.Icc x y ⊆ Set.Icc a b) (z : ℝ) (m : x ≤ z ∧ z ≤ y) :
+    let (r, s) := forward q x y h
+    r ≤ f z ∧ f z ≤ s
 
-@[simp]
-theorem iteratedDeriv_even_cos (n : ℕ) :
-    iteratedDeriv (2 * n) Real.cos = (-1) ^ n * Real.cos := by
-  induction n with
-  | zero => simp
-  | succ n ih => simp_all [mul_add, pow_succ]
+/-!
+Notes:
+* There'll be an internal version of this that uses `Dyadic` rather than `ℚ`,
+  but we can automatically build one from the other.
+* The machinery that uses this will not know anything about `ℝ`, which lives in Mathlib.
+  Instead, we will have a typeclass for "has order comparisons with `Dyadic`",
+  which Lean will provide for `ℚ` and Mathlib will provide for `ℝ`.
+* There will also be fancier versions that
+  * allow for multiple arguments
+  * can maintain state for subsequent calls at higher quality
+  * can give suggestions about how to best split the input interval
+-/
 
-theorem iteratedDeriv_odd_sin (n : ℕ) :
-    iteratedDeriv (2 * n + 1) Real.sin = (-1) ^ n * Real.cos := by simp
+/-!
+We'll also have a user level gadget that says:
+if you have an interval that doesn't lie inside `Set.Icc (-1) 1`
+that you're trying to propagate through `sin`,
+add the identity `sin x = 3 * sin (x / 3) - 4 * (sin (x / 3)) ^ 3` instead.
+This has the effect of shrinking the interval by a factor of 3,
+at the expense of having to push the resulting estimate through the `3 * y - 4 * y^3` function.
+Recursing, we can shrink into the interval `Set.Icc (-1) 1`.
+-/
 
-@[simp]
-theorem iteratedDeriv_odd_cos (n : ℕ) :
-    iteratedDeriv (2 * n + 1) Real.cos = (-1) ^ (n + 1) * Real.sin := by simp [pow_succ]
-
-theorem differentiable_iteratedDeriv_sin (n : ℕ) :
-    Differentiable ℝ (iteratedDeriv n Real.sin) :=
-  match n with
-  | 0 => by simp
-  | 1 => by simp
-  | n + 2 => by simp [differentiable_iteratedDeriv_sin]
-
-theorem differentiable_iteratedDeriv_cos (n : ℕ) :
-    Differentiable ℝ (iteratedDeriv n Real.cos) :=
-  match n with
-  | 0 => by simp
-  | 1 => by simp
-  | n + 2 => by simp [differentiable_iteratedDeriv_cos]
-
-theorem abs_iteratedDeriv_sin_le_one (n : ℕ) (x : ℝ) :
-    |iteratedDeriv n Real.sin x| ≤ 1 :=
-  match n with
-  | 0 => by simpa using Real.abs_sin_le_one x
-  | 1 => by simpa using Real.abs_cos_le_one x
-  | n + 2 => by simpa using abs_iteratedDeriv_sin_le_one n x
-
-theorem abs_iteratedDeriv_cos_le_one (n : ℕ) (x : ℝ) :
-    |iteratedDeriv n Real.cos x| ≤ 1 :=
-  match n with
-  | 0 => by simpa using Real.abs_cos_le_one x
-  | 1 => by simpa using Real.abs_sin_le_one x
-  | n + 2 => by simpa using abs_iteratedDeriv_cos_le_one n x
-
-end
-
-section -- https://github.com/leanprover-community/mathlib4/pull/29024
-
-theorem iteratedDerivWithin_Ioo {n : ℕ} {a b : ℝ} (h : a < b) {x : ℝ} (hx : x ∈ Set.Ioo a b)
-    {f : ℝ → ℝ} (hf : ContDiffOn ℝ n f (Set.Icc a b)) :
-    iteratedDerivWithin n f (Set.Ioo a b) x = iteratedDeriv n f x := by
-  rw [iteratedDerivWithin_eq_iteratedDeriv]
-  · exact uniqueDiffOn_convex (convex_Ioo a b) (by simp_all)
-  · exact ContDiffOn.contDiffAt hf (x := x) (by rwa [Icc_mem_nhds_iff])
-  · exact hx
-
-theorem iteratedDerivWithin_Icc {n : ℕ} {a b : ℝ} (h : a < b) {x : ℝ} (hx : x ∈ Set.Icc a b)
-    {f : ℝ → ℝ} (hf : ContDiff ℝ n f) :
-    iteratedDerivWithin n f (Set.Icc a b) x = iteratedDeriv n f x := by
-  rw [iteratedDerivWithin_eq_iteratedDeriv]
-  · exact uniqueDiffOn_convex (convex_Icc a b) (by simp_all)
-  · exact ContDiff.contDiffAt hf
-  · exact hx
-
-@[simp]
-theorem iteratedDerivWithin_sin_Icc (n : ℕ) {a b : ℝ} (h : a < b) {x : ℝ} (hx : x ∈ Set.Icc a b) :
-    iteratedDerivWithin n Real.sin (Set.Icc a b) x = iteratedDeriv n Real.sin x :=
-  iteratedDerivWithin_Icc h hx Real.contDiff_sin
-
-@[simp]
-theorem iteratedDerivWithin_cos_Icc (n : ℕ) {a b : ℝ} (h : a < b) {x : ℝ} (hx : x ∈ Set.Icc a b) :
-    iteratedDerivWithin n Real.cos (Set.Icc a b) x = iteratedDeriv n Real.cos x :=
-  iteratedDerivWithin_Icc h hx Real.contDiff_cos
-
-end
+/-!
+We now give an example of a forward propagator for `sin` using Taylor polynomials.
+-/
 
 namespace Real.sin
 
@@ -131,9 +78,6 @@ def ratErrorBound (n : ℕ) (x : ℚ) : ℚ :=
 theorem iteratedDerivAtZero_eq (n : ℕ) :
     iteratedDerivAtZero n = iteratedDeriv n sin 0 :=
   match n with | 0 | 1 | n + 2 => by simp [iteratedDerivAtZero, iteratedDerivAtZero_eq]
-
--- https://github.com/leanprover-community/mathlib4/pull/29026
-attribute [simp] PolynomialModule.eval_smul
 
 theorem ratApprox_coe_eq {x : ℚ} {n : ℕ} (h : 0 < x) :
     (ratApprox n x : ℝ) = taylorWithinEval sin n (Set.Icc 0 ↑x) 0 ↑x := by
@@ -236,27 +180,6 @@ theorem mem_interval (n : ℕ) {x y : ℚ} (wx : -1 ≤ x) (wy : y ≤ 1) {z : �
   unfold upperBound
   grind [abs_le, Rat.cast_sub, Rat.cast_add]
 
-end Real.sin
-
-/--
-A machine for propagating intervals through a function.
-
-The propagator is only required to propagate subintervals of `Set.Icc a b`.
-
-* `forward q x y h` must produce a pair of rationals `(r, s)` such that
-  if `z ∈ Set.Icc x y` then `f x ∈ Set.Icc r s`
-
-The parameter `q` is a "quality" parameter, which is used to control the effort expended in
-producing a tight interval.
-No guarantees are required regarding the dependence on `q`,
-but we suggest that where possible the additional error introduced should be bounded by `q`.
--/
-structure IntervalPropagator (f : ℝ → ℝ) (a b : ℚ) where
-  forward (q : ℚ) (x : ℚ) (y : ℚ) (h : Set.Icc x y ⊆ Set.Icc a b) : ℚ × ℚ
-  mem (q : ℚ) (x : ℚ) (y : ℚ) (h : Set.Icc x y ⊆ Set.Icc a b) (z : ℝ) (m : x ≤ z ∧ z ≤ y) :
-    let (r, s) := forward q x y h
-    r ≤ f z ∧ f z ≤ s
-
 /--
 An interval propagator for `sin` on the interval `Set.Icc (-1) 1`.
 
@@ -267,15 +190,40 @@ So if we take `n = (q⁻¹).ceil.toNat.log2`, the error will be bounded by `q`.
 One could do better (i.e. get away with a smaller `n`) by bounding the factorial more tightly,
 or using the input interval, which may be closer to zero.
 -/
-def Real.sin.propagator : IntervalPropagator Real.sin (-1) 1 where
+def propagator : IntervalPropagator Real.sin (-1) 1 where
   forward q x y h := Real.sin.interval (q⁻¹).ceil.toNat.log2 x y
   mem q x y h z m :=
     have h := (Set.Icc_subset_Icc_iff (by rify; grind)).mp h
     Real.sin.mem_interval (q⁻¹).ceil.toNat.log2 h.1 h.2 m
 
--- We'll also have a user level gadget that says:
--- if you have an interval that doesn't lie inside `Set.Icc (-1) 1`
--- that you're trying to propagate through `sin`,
--- add the identity `sin x = 3 sin (x / 3) - 4 * (sin (x / 3)) ^ 3` instead.
--- This has the effect of shrinking the interval by a factor of 3:
--- recursing, we can shrink into the interval `Set.Icc (-1) 1`.
+end Real.sin
+
+
+
+/-!
+We can build forward propagators for functions which have rational approximations and a computable
+modulus of continuity, but I haven't implemented this yet.
+
+Suppose we have a function $f : {\mathbb R} \to {\mathbb R}$, together with
+* a rational approximation $\tilde f : {\mathbb Q} \times {\mathbb Q_{>0}} \to \mathbb Q$ satisfying
+  $$|\tilde f(x, \epsilon) - f(x)| < \epsilon$$
+* and a (computable) modulus of uniform continuity
+  $\mu : {\mathbb Q_{>0}} \to {\mathbb Q_{>0}} \cup \{\infty\}$ satisfying
+  $$|x - y| < \mu(\epsilon) \rightarrow |f(x) - f(y)| < \epsilon.$$
+
+Then we can construct a forward propagator for $f$ with quality $\epsilon \in {\mathbb Q}_{>0}$
+which takes an input interval $[a, b]$ and produces an output interval by:
+* subdividing into $a = a_0 < a_1 < \cdots < a_k = b$ so that $|a_{i+1} - a_i| < 2\mu(\epsilon)$
+* let $c = \operatorname{min} \{\tilde{f}(a_i, \epsilon)\}$,
+  and $d = \operatorname{max} \{\tilde{f}(a_i, \epsilon)\}$
+* returning the interval $[c-2\epsilon, d+2\epsilon]$
+* and the proof that if $x \in [a, b]$, then $|x-a_i|< \mu(\epsilon)$ for some $i$,
+  so $|f(x)-f(a_i)| < \epsilon$ and hence $|f(x) - \tilde{f}(a_i, \epsilon)| < 2\epsilon$
+  and thus $f(x) \in [c-2\epsilon, d+2\epsilon]$
+
+The modulus of continuity may be allowed to depend on the input interval $[a,b]$.
+
+(As an example of a forward propagator maintaining state,
+when run on a smaller interval we could refine the previous subdivision
+and reuse previously computed values of $\tilde{f}$.)
+-/
